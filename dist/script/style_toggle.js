5786d1f9e87cbba9c5e1613adf52f2b8a1ce3513
Aaron Pinero <aaronpinero@gmail.com>
2018-02-08 21:49:14 -0500

    working on documentation and examples

--- dist/script/style_toggle.js
+++ dist/script/style_toggle.js
@@ -1,13 +1,16 @@
 theme_tyfy = "../dist/style/tyfy-bootstrap.css";
+theme_tyfy_docs = "../dist/style/tyfy-bootstrap-docs.css";
 theme_default = "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css";
 $('#style-toggle').click(function(e){
 	e.preventDefault();
 	link = $('link#mainstyles');
+	linkDocs = $('link#docstyles');
 	link_href = link.attr('href');
 	if (link_href == theme_tyfy) {
 		link.attr('href',theme_default);
 	}
 	else {
 		link.attr('href',theme_tyfy);
+		linkDocs.attr('href',theme_tyfy_docs);
 	}
 });
