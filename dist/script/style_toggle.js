3628e32f3231bcfa85d267a5812c471d828365b5
Aaron Pinero <aaronpinero@gmail.com>
2019-10-11 16:16:30 -0400

    customizing docs fonts; simplifying stylesheet structure

--- dist/script/style_toggle.js
+++ dist/script/style_toggle.js
@@ -1,16 +1,13 @@
-theme_tyfy = "../dist/style/tyfy-bootstrap.css";
-theme_tyfy_docs = "../dist/style/tyfy-bootstrap-docs.css";
+theme_tyfy= "../dist/style/tyfy-bootstrap-docs.css";
 theme_default = "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css";
 $('#style-toggle').click(function(e){
 	e.preventDefault();
-	link = $('link#mainstyles');
-	linkDocs = $('link#docstyles');
+	link = $('link#docstyles');
 	link_href = link.attr('href');
 	if (link_href == theme_tyfy) {
 		link.attr('href',theme_default);
 	}
 	else {
 		link.attr('href',theme_tyfy);
-		linkDocs.attr('href',theme_tyfy_docs);
 	}
 });
