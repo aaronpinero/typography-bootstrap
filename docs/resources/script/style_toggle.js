4c8b57fa0b27fff51761e573f186ab758edc7d7f
Aaron Pinero <aaronpinero@gmail.com>
2021-03-06 23:35:05 -0500

    switch to new version of bootstrap

--- docs/resources/script/style_toggle.js
+++ docs/resources/script/style_toggle.js
@@ -1,5 +1,5 @@
 theme_tyfy= "resources/style/tyfy-bootstrap-docs.css";
-theme_default = "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css";
+theme_default = "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css";
 $('#style-toggle').click(function(e){
 	e.preventDefault();
 	link = $('link#docstyles');
