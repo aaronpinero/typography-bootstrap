a44e28c2549c0ad734a3baa984d04f9719447a38
Aaron Pinero <aaronpinero@gmail.com>
2021-03-11 23:04:51 -0500

    updated include files for docs; updated scripts for docs; updated docs home page html

--- docs/resources/script/nav_active.js
+++ docs/resources/script/nav_active.js
@@ -1,9 +1,9 @@
-$(document).ready(function(){
-	current_page = window.location.href;
-	$('.navbar a').each(function(){
-		url = $(this).attr('href');
-		if (current_page.indexOf(url) != -1) {
-			$(this).addClass('active');
-		}
-	});
+
+var current_page = window.location.href;
+var nav_links = document.querySelectorAll('.navbar-tybs .nav-link');
+nav_links.forEach(function(link){
+	var url = link.getAttribute('href');
+	if (current_page.indexOf(url) != -1) {
+		link.classList.add('active');
+	}
 });
--- docs/resources/script/style_toggle.js
+++ docs/resources/script/style_toggle.js
@@ -1,13 +1,14 @@
-theme_tyfy= "resources/style/tyfy-bootstrap-docs.css";
-theme_default = "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css";
-$('#style-toggle').click(function(e){
+var theme_tyfy = "resources/style/tyfy-bootstrap-docs.css";
+var theme_default = "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css";
+var style_toggle = document.querySelector('#style-toggle');
+style_toggle.addEventListener('click',function(e){
 	e.preventDefault();
-	link = $('link#docstyles');
-	link_href = link.attr('href');
-	if (link_href == theme_tyfy) {
-		link.attr('href',theme_default);
+	var stylesheet_link = document.querySelector('#docstyles');
+	var stylesheet_link_href = stylesheet_link.getAttribute('href');
+	if (stylesheet_link_href == theme_tyfy) {
+		stylesheet_link.setAttribute('href',theme_default);
 	}
 	else {
-		link.attr('href',theme_tyfy);
+		stylesheet_link.setAttribute('href',theme_tyfy);
 	}
 });
